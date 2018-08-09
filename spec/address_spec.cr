3f7f8f32f246f931460482acd0c91889b3d410dc
John Doe <ian.purton+c9@gmail.com>
2018-08-09 13:02:17 +0000

    Tets multi sig.

--- spec/address_spec.cr
+++ spec/address_spec.cr
@@ -25,7 +25,7 @@ describe OnChain::API::Address do
       "ubrBeFazVb", 
       "m/0/0"
       
-    keys = OnChain::API::ExtendedKeys.new 1, xpubs
+    keys = OnChain::API::ExtendedKeys.new 0, xpubs
   
     resp = OnChain::API::Address.to_network_addresses(keys)
   
@@ -33,6 +33,34 @@ describe OnChain::API::Address do
     when OnChain::API::NetworkAddresses
       resp.addresses.first.network_address.should eq(
         "1NJPSsYiAyikaGd97y4G5WgmWkWbVSoXwj")
+    else
+      puts resp.to_s
+      true.should eq(false)
+    end
+  end
+  
+  it "should convert multi sig xpubs to addresses" do
+  
+    xpubs = Array(OnChain::API::ExtendedKey).new 
+  
+    xpubs << OnChain::API::ExtendedKey.new "xpub68HJZ9inzbsZgSy8R2AwTWh7cxzvt" +
+      "NKYFEXrAszoZjwMnXvrAGto3qcB9ScwZpDRQxXobNa4nb3uDqHSYnBKPoQjonLXFcjYk" +
+      "ubrBeFazVb", 
+      "m/0/0"
+  
+    xpubs << OnChain::API::ExtendedKey.new "xpub68HJZ9inzbsZgSy8R2AwTWh7cxzvt" +
+      "NKYFEXrAszoZjwMnXvrAGto3qcB9ScwZpDRQxXobNa4nb3uDqHSYnBKPoQjonLXFcjYk" +
+      "ubrBeFazVb", 
+      "m/0/1"
+      
+    keys = OnChain::API::ExtendedKeys.new 1, xpubs
+  
+    resp = OnChain::API::Address.to_network_addresses(keys)
+  
+    case resp
+    when OnChain::API::NetworkAddresses
+      resp.addresses.first.network_address.should eq(
+        "32st1ZWPSYoe9dRSQnTW3eMVuKNnddfFE4")
     else
       puts resp.to_s
       true.should eq(false)
